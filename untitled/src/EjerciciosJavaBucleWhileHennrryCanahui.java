0da970b5fdb6559e751cff32ba4d2fe4e4c84bea
hennrry <hennrrycanahuiholasoy@gmail.com>
2024-05-25 08:02:13 -0600

    Ejercicios Bucle while

--- untitled/src/EjerciciosJavaBucleWhileHennrryCanahui.java
+++ untitled/src/EjerciciosJavaBucleWhileHennrryCanahui.java
@@ -1,2 +1,62 @@
+import java.util.Scanner;
+
 public class EjerciciosJavaBucleWhileHennrryCanahui {
+    public String nombre;
+    public EjerciciosJavaBucleWhileHennrryCanahui(){
+
+    }
+
+    public static void imprimirNumerosAscendentes() {
+        int numero = 1;
+        while (numero <= 20) {
+            System.out.print(numero + " ");
+            numero++;
+        }
+    }
+
+    public boolean ListaNum(int a) {
+        int inicio = 1;
+        while (inicio <= a) {
+            System.out.println(inicio);
+            inicio++;
+        }
+        return false;
+    }
+
+    public static void sumarNumerosPositivos(Scanner nums) {
+        int suma = 0;
+        int numero;
+
+        System.out.println("Ingresa números positivos (ingresa un número negativo para detener):");
+        while (true) {
+            numero = nums.nextInt();
+            if (numero < 0) {
+                break;
+            }
+            suma += numero;
+        }
+
+        System.out.println("La suma de los números positivos es: " + suma);
+    }
+
+    public static void generarSecuenciaFibonacci(int limite) {
+        int a = 0, b = 1;
+        System.out.print("Secuencia de Fibonacci hasta " + limite + ": ");
+        while (a <= limite) {
+            System.out.print(a + " ");
+            int siguiente = a + b;
+            a = b;
+            b = siguiente;
+        }
+    }
+
+
+
+
+
+
+
 }
+
+
+
--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("ingresa el numero a identificar:");
@@ -13,14 +14,14 @@ public class Main {
         EjerciciosJavaIf_elseHennrryCanahui identificacion = new EjerciciosJavaIf_elseHennrryCanahui();
         System.out.println(identificacion.identificadorNumero(num));
 
-    //
+        //
 
         System.out.println("ingresa el numero a identificar:");
         int numeroParImpar = scanner.nextInt();
         EjerciciosJavaIf_elseHennrryCanahui numParImpar = new EjerciciosJavaIf_elseHennrryCanahui();
         System.out.println(numParImpar.identificadorNumero(numeroParImpar));
 
-    //
+        //
         System.out.println("ingresa primer numero:");
         int primerNum = scanner.nextInt();
         System.out.println("ingresa segundo numero:");
@@ -28,17 +29,38 @@ public class Main {
         System.out.println("ingresa tercer numero:");
         int tercerNum = scanner.nextInt();
         EjerciciosJavaIf_elseHennrryCanahui comparadorNumeros = new EjerciciosJavaIf_elseHennrryCanahui();
-        System.out.println(comparadorNumeros.numeroMayor(primerNum,segundoNum, tercerNum));
+        System.out.println(comparadorNumeros.numeroMayor(primerNum, segundoNum, tercerNum));
 
-    //
+        //
 
         System.out.print("Ingresa un año: ");
         int año = scanner.nextInt();
         EjerciciosJavaIf_elseHennrryCanahui añoBisiesto = new EjerciciosJavaIf_elseHennrryCanahui();
         System.out.println(añoBisiesto.añoBisiesto(año));
 
+        //
+
+        EjerciciosJavaBucleWhileHennrryCanahui unoAlVeingte = new EjerciciosJavaBucleWhileHennrryCanahui();
+        System.out.println(unoAlVeingte);
+
+        //
 
 
+        System.out.print("Ingresa un numero mayor a 1: ");
+        int hastaNum = scanner.nextInt();
+        EjerciciosJavaBucleWhileHennrryCanahui listaNum = new EjerciciosJavaBucleWhileHennrryCanahui();
+        System.out.println(listaNum.ListaNum(hastaNum));
+
+        //
+        Scanner scanner_dos = new Scanner(System.in);
+        EjerciciosJavaBucleWhileHennrryCanahui.sumarNumerosPositivos(scanner_dos);
+
+
+        //
+        Scanner scanner_tres = new Scanner(System.in);
+        System.out.print("Ingresa un número: ");
+        int limite = scanner_tres.nextInt();
+        EjerciciosJavaBucleWhileHennrryCanahui.generarSecuenciaFibonacci(limite);
 
 
 
